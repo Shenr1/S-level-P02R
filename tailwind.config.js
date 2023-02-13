d1a3380baf5b1c10f55fa8a0cfcc34479db90c44
ryen <ysw@ink-bird.com>
2023-02-13 12:07:55 +0800

    banner完成

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        viGreen:{
+        light2: '#6dba3d',
+        light1: '#6dba3d',
+        DEFAULT: '#93c90f',
+        dark1: '#93c90f',
+        dark2: '#93c90f'
+        },
+        vi:{
+          light: '#ff7ce5',
+          DEFAULT: '#0080b9',
+          dark: '#ff16d1'
+        }
+      },
+      fontSize: {
+        'xxl': ['10rem' , 'initial'],
+        '2xxl': ['12rem' , 'initial'],
+        '3xxl': ['14rem' , 'initial'],
+        '4xxl': ['16rem' , 'initial'],
+        '5xxl': ['18rem' , 'initial'],
+        '6xxl': ['20rem' , 'initial']
+      },
+      fontFamily: {
+        'Impact': 'Impact, Arial, sans-serif',
+        'Montserrat': 'Montserrat, Arial, sans-serif',
+        'Sans': 'Noto Sans SC, Arial, sans-serif'
+      }
+    },
   },
   plugins: [],
 }
